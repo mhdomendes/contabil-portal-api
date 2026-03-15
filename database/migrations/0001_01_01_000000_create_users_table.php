8b1c5d6254a342576ea8df322b2669f79aaaa3a9
MatheusMendes <matheusgbe.mendes@outlook.com>
2026-03-15 09:44:51 -0300

    feat: Migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('accountant_id')->nullable()->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->enum('role', ['accountant', 'client'])->default('client');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
